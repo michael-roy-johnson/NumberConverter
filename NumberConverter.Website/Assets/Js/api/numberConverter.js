ca15e14467785dde504736348995ea4ce94fdf47
Michael Johnson <michael.johnson@ie.com.au>
2018-10-11 00:27:50 +1100

    Server port change

--- NumberConverter.Website/Assets/Js/api/numberConverter.js
+++ NumberConverter.Website/Assets/Js/api/numberConverter.js
@@ -2,7 +2,7 @@
 
 export default class NumberConverter {
     static GetNumberToWords(number) {
-        return Axios.get('http://localhost:53437/api/converter/' + number)
+        return Axios.get('http://localhost:5000/api/converter/' + number)
             .then(res => {
                 return res;
             });
